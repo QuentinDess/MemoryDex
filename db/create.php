ff8bbd36a92bd2e21c2cb0f71e11ea9e3016f537
SolineMP <56126066+SolineMP@users.noreply.github.com>
2019-11-22 17:03:43 +0100

    correctifs

--- db/create.php
+++ db/create.php
@@ -4,25 +4,14 @@ $userRole = 2;
 
 // Initialiser une connexion à notre DB (DataBase)
 include 'dbconnexion.php';
-
- /*echo password_hash($_POST['password'], PASSWORD_DEFAULT);*/
-
-
-// $db = "INSERT INTO User(name, password) VALUES (':name', ':password')";
+// Requête SQL
 $db = "INSERT INTO Users(name, password, id_Roles) VALUES (:name, :password, :discriminant);";
-
-
+// Préparation de la requête
 $createUser = $dbConnection->prepare($db);
-
-
 $createUser->bindParam(":name", $_POST['name'], PDO::PARAM_STR);
 $createUser->bindParam(":password", $_POST['password']);
 $createUser->bindParam(":discriminant", $userRole);
-
-
+// Exécution de la requête
 $createUser->execute();
-
-// var_dump($createUser);die;
-
 header('Location: ../index.php');
 
