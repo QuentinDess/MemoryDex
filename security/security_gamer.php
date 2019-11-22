b42131728a5a8ff9e1d7ae10703900e8c9bbeb96
ratshin38 <52753337+ratshin38@users.noreply.github.com>
2019-11-22 15:56:36 +0100

    comment

--- security/security_gamer.php
+++ security/security_gamer.php
@@ -1,4 +1,5 @@
 <?php 
+// permet d'empêcher un utilisateur non connecté d'accéder à certaines pages
                 if (!isset($_SESSION) or empty($_SESSION)){
                     header('Location: ../index.php');
                 } 
