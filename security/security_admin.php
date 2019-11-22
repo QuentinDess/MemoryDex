dbabb803f779b0466a8d78f2e6e880b4652c28ad
ratshin38 <52753337+ratshin38@users.noreply.github.com>
2019-11-22 15:55:36 +0100

    comment

--- security/security_admin.php
+++ security/security_admin.php
@@ -1,4 +1,5 @@
 <?php 
+//permet d'empêcher un utilisateur non connecté et donc le role n'est pas d'admin d'accéder au page ou ce code est include
                 if (!isset($_SESSION) or empty($_SESSION)){
                     header('Location: ../index.php');
                 } 
